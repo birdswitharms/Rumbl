a60737bea8f8bb138674a555f2be3c44b475eb2f
ANDREW SEGAL <segal.a@hotmail.com>
2018-07-17 17:27:33 -0400

    Add login UI to app nav, and logout method

--- lib/rumbl_web/controllers/session_controller.ex
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -1,12 +1,14 @@
 defmodule RumblWeb.SessionController do
   use RumblWeb, :controller
 
+  alias RumblWeb.Auth
+
   def new(conn, _) do
     render(conn, "new.html")
   end
 
   def create(conn, %{"session" => %{"email" => email, "password" => pass}}) do
-    case RumblWeb.Auth.login_by_email_and_pass(conn, email, pass) do
+    case Auth.login_by_email_and_pass(conn, email, pass) do
       {:ok, conn} ->
         conn
         |> put_flash(:info, "Welcome back!")
@@ -16,5 +18,11 @@ defmodule RumblWeb.SessionController do
         |> put_flash(:error, "Invalid email or password")
         |> render("new.html")
     end
+  end
+
+  def delete(conn, _) do
+    conn
+    |> Auth.logout()
+    |> redirect(to: page_path(conn, :index))
   end
 end
